65e769efcd96abd4b9d94c8f6402120c70709153
Tony <Tony_zherka@hotmail.co.uk>
2022-04-04 15:03:25 +0000

    Tz: changes made to models

--- applications/models.py
+++ applications/models.py
@@ -13,12 +13,12 @@ class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     genre = db.Column(db.String(40), nullable=False)
-    dev = db.Column(db.String(50), nullable=False)
+    developer = db.Column(db.String(50), nullable=False)
     reviews = db.relationship('Review', backref='game')
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     content = db.Column(db.String(4000), nullable=False)
-    date = db.Column(db.DateTime)
+    time = db.Column(db.DateTime)
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
